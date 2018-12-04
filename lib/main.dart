fda145a7d216ca14412278f6d91992ec1a534c44
wjw0215 <821060818@qq.com>
2018-12-04 21:53:26 +0800

    组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'dart:convert';
+//import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+
 void main() => runApp(new FlutterApp());
 
 
@@ -8,14 +12,53 @@ class FlutterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: '任务页面标题',
-      home: new Scaffold(
+      home: new DemoFlutter(),
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: new Text('tabbar'),
+//        ),
+//        body: new Center(
+//          child: new Text('内容'),
+//        ),
+//      ),
+    );
+  }
+}
+
+class DemoFlutter extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    // ignore: expected_token
+    return new DemoFlutterState();
+  }
+//   ignore: missing_method_parameters
+//  createState => new DemoFlutterState();
+
+}
+
+class DemoFlutterState extends State<DemoFlutter> {
+  var _list = [];
+  //添加一个属性来保存文本的样式
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  _loadData() async {
+    String dataURL = "https://api.github.com/orgs/raywenderlich/members";
+//    http.Response response = await http.get(dataURL);
+    Dio dio = new Dio();
+    Response response = await dio.get(dataURL);
+    setState(() {
+      _list = JSON.decode(response.body);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold (
         appBar: new AppBar(
-          title: new Text('tabbar'),
-        ),
-        body: new Center(
-          child: new Text('内容'),
-        ),
+        title: new Text("新的组件"),
       ),
+      body: new Text("新的组件"),
     );
   }
 }
