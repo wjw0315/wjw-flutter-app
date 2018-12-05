62e42e0c0ea17bfcad907a7ebea05f118466783d
wjw0215 <821060818@qq.com>
2018-12-05 11:50:35 +0800

    dio，http请求获取数据，ListView

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class FlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: '任务页面标题',
+      title: 'FlutterAPP',
       home: new DemoFlutter(),
 //      home: new Scaffold(
 //        appBar: new AppBar(
@@ -42,23 +42,50 @@ class DemoFlutterState extends State<DemoFlutter> {
   //添加一个属性来保存文本的样式
   final _biggerFont = const TextStyle(fontSize: 18.0);
   _loadData() async {
-    String dataURL = "https://api.github.com/orgs/raywenderlich/members";
+    String dataURL = "https://api.github.com/users";
 //    http.Response response = await http.get(dataURL);
     Dio dio = new Dio();
     Response response = await dio.get(dataURL);
     setState(() {
-      _list = JSON.decode(response.body);
+      _list = response.data;
     });
   }
+
+  /**
+   * 重写initSate方法，在组件初始化时调用_loadData方法
+   */
+  @override
+  void initState(){
+    super.initState();
+    _loadData();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold (
         appBar: new AppBar(
-        title: new Text("新的组件"),
+        title: new Text("FlutterAPP"),
       ),
-      body: new Text("新的组件"),
+//      body: new Text("新的组件"),
+        body: new ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemCount: _list.length,
+            itemBuilder: (BuildContext context, int position) {
+              return _buiddList(position);
+            }),
     );
+  }
+
+  /**
+   * 使用ListView组件显示_loadData方法中获取到的list数据
+   */
+  Widget _buiddList(int i){
+    //返回一个ListTile组件，该组件显示第iJSON成员的"login"值，
+    // 还使用您之前创建的文本样式(_biggerFont)
+      return new ListTile(
+          title : new Text("${_list[i]["login"]}", style: _biggerFont)
+      );
   }
 }
